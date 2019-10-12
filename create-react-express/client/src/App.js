daeaa886fb17015932dfef4658c5beffffd48327
Joscandy Nunez <50030017+jcandyn@users.noreply.github.com>
2019-10-12 03:00:28 -0400

    connected app to heroku

---
+++ create-react-express/client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Jumbotron from './components/Jumbotron'
+import Game from './components/Game'
+import './App.css';
+
+
+class App extends React.Component {
+
+      render() {
+  return (
+    <div className="container-fluid">
+    <Jumbotron/>
+   <Game/>
+   </div>
+  );
+}
+}
+
+export default App;
---
+++ create-react-express/client/src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../Card.css";
+
+class Card extends React.Component {
+
+  
+    render() {
+        return(
+        
+        <div className="col-md-2" onClick = {() => { this.props.handleIncrement(this.props.id); 
+        this.props.shuffle(this.props.id)
+        ; }} >
+        <img  src={this.props.thisLink} alt="image of cartoon"/>
+      </div>
+        )
+    }
+  }
+  
+  export default Card
